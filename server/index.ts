0c3b176484246dd0852b75398518316cd0655b5e
Lavina-23 <lavinakohai3227@gmail.com>
2024-08-06 10:23:32 +0700

    fix: change route file

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import cors from "cors";
+import express from "express";
+import records from "./src/routes/record";
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/record", records);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
